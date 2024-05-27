19f9857b519ba0db58ff1dd94217e6b7ec77a2b8
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-27 04:45:00 -0300

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Home from "../routes/home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation component</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/crownJSX" element={<Home />} />
+      <Route path="/crownJSX" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
