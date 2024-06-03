6a5a17b738f9e2398e0ada03e74140b035ca5b12
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-06-02 23:29:30 -0300

    created product component and container

--- routes/shop/index.jsx
+++ routes/shop/index.jsx
@@ -1,12 +1,14 @@
-import SHOP_DATA from "../../src/shop-data.json";
+import { useContext } from "react";
+import { ProductsContext } from "../../src/contexts/products.context";
+import ProductCard from "../../src/components/product-cart";
+import "./styles.scss"
 
 export default function Shop() {
+  const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {SHOP_DATA.map(({ id, name }) => (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+    <div className="products-container" >
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
