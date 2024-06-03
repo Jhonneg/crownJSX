db811bdd8683af0c2e1252fd02f7a27ce510c196
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-06-03 20:13:03 -0300

    added cart item component and context

--- src/components/cart-dropdown/index.jsx
+++ src/components/cart-dropdown/index.jsx
@@ -1,12 +1,16 @@
 import "./styles.scss";
 import Button from "../button";
+import CartItem from "../cart-item";
 
 export default function CardDropdown() {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>CHECKOUT</Button>
+        {[].map((item) => (
+          <CartItem cartItem={item} />
+        ))}
       </div>
+      <Button>CHECKOUT</Button>
     </div>
   );
 }
---
+++ src/components/cart-item/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+export default function CartItem({ cartItem }) {
+  const { name, quantity } = cartItem;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <span>{quantity}</span>
+    </div>
+  );
+}
