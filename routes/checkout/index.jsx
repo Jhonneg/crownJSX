e59935c490951edae9744fbf2c34d71f58c5a88a
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-06-07 20:05:57 -0300

    expanded checkout component

--- routes/checkout/index.jsx
+++ routes/checkout/index.jsx
@@ -1,9 +1,23 @@
+import { useContext } from "react";
+import { CartContext } from "../../src/contexts/cart.context";
 import "./styles.scss";
 
 export default function Checkout() {
+  const { cartItems } = useContext(CartContext);
   return (
     <div>
       <h1>i am the checkout page</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
