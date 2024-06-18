a99fc92e00a03659d3ab34d49770e78f6a5ae84c
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-06-18 12:12:51 -0300

    added reducer utils into user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -3,6 +3,8 @@ import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
+
 //actual value you want to access
 export const UserContext = createContext({
   currentUser: null,
@@ -37,7 +39,7 @@ export const UserProvider = ({ children }) => {
   console.log(currentUser);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
   const value = { currentUser, setCurrentUser };
 
